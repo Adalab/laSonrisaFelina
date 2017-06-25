c9feaf81096328a68288023e7a4e079b1af5542b
Marta Bermejo <m.berlo8583@gmail.com>
2017-06-25 13:17:24 +0200

    Added links and jquery to move to the section

--- js/main.js
+++ js/main.js
@@ -22,6 +22,26 @@ function changeOpacity(){
   }
 }
 
+$(function(){
+
+   $('a.link-nav').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+                 var targetOffset = $target.offset().top;
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+                 return false;
+          }
+      }
+   });
+});
+
 //Calculadora
 var resultButton = document.getElementById("mlResult");
 var mgProductElement = document.getElementById("mgProduct");
